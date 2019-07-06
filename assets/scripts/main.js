49fe91f02f0916966588e624fd173d1e0a7f679d
Senadin <senadin.botic@gmail.com>
2019-07-07 00:39:17 +0200

    Checklist

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -370,7 +370,124 @@ $(document).ready(function () {
     $('.tab_accord_wrapper .tabs li').on('click', function () {
         $('.tab_content').removeClass('show-more')
     })
+
+    // Product Checkout
+    $('.checkout-btn').on('click', function () {
+        $('.checkout-container').addClass('checkout-active');
+    })
+    $('.close-checkout').on('click', function () {
+        $('.checkout-container').removeClass('checkout-active').removeClass('category-active');
+        $('.category-container').removeClass('categoryItem');
+    });
+    $('.additional-item').on('click', function () {
+        $('.checkout-container').addClass('category-active');
+    });
+    $('.new-additional-item .second-btn').on('click', function () {
+        var txt = $(this).parent().hasClass('active') ? 'TILFØJ' : 'TILFØJET';
+        $(this).text(txt);
+        $(this).parent().toggleClass('active');
+        $('.checkout-container').addClass('category-active');
+    });
+    $('.back-button').on('click', function () {
+        $('.checkout-container').removeClass('category-active');
+        $('.category-container').removeClass('categoryItem');
+
+        additionalItem();
+    });
+    $(".additional-item").click(function () {
+        var activeCategory = $(this).attr("rel");
+        $("#" + activeCategory).addClass('categoryItem');
+    });
+
+    additionalItem();
+
+    // Plus/Minus Product Buttons
+    $('.btn-number').click(function (e) {
+        e.preventDefault();
+
+        fieldName = $(this).attr('data-field');
+        type = $(this).attr('data-type');
+        var input = $("input[name='" + fieldName + "']");
+        var currentVal = parseInt(input.val());
+        if (!isNaN(currentVal)) {
+            if (type == 'minus') {
+
+                if (currentVal > input.attr('min')) {
+                    input.val(currentVal - 1).change();
+                }
+                if (parseInt(input.val()) == input.attr('min')) {
+                    $(this).attr('disabled', true);
+                }
+
+            } else if (type == 'plus') {
+
+                if (currentVal < input.attr('max')) {
+                    input.val(currentVal + 1).change();
+                }
+                if (parseInt(input.val()) == input.attr('max')) {
+                    $(this).attr('disabled', true);
+                }
+
+            }
+        } else {
+            input.val(0);
+        }
+    });
+    $('.input-number').focusin(function () {
+        $(this).data('oldValue', $(this).val());
+    });
+    $('.input-number').change(function () {
+
+        minValue = parseInt($(this).attr('min'));
+        maxValue = parseInt($(this).attr('max'));
+        valueCurrent = parseInt($(this).val());
+
+        name = $(this).attr('name');
+        if (valueCurrent >= minValue) {
+            $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+        if (valueCurrent <= maxValue) {
+            $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+
+
+    });
+    $(".input-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+            // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
 });
+
+// Choose Product Additional Item
+function additionalItem() {
+    $('.category-container').each(function() {
+        if( $(this).children().hasClass('active') ) {
+            $(this).addClass('choosen-category');
+            var selectItem = $(this).attr('id');
+            $(".additional-item[rel^='" + selectItem + "']").addClass('active');
+        }
+        else {
+            $(this).removeClass('choosen-category');
+            var selectItem = $(this).attr('id');
+            $(".additional-item[rel^='" + selectItem + "']").removeClass('active');
+        }
+    });
+}
 
 //Set Map
 function initMap() {
